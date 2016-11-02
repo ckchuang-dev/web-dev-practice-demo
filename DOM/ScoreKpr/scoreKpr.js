61cc71abb19fa475fafaf898a720646a967637f1
dez_chuang <dez_chuang@compal.com>
2016-11-02 21:16:27 +0800

    [DOM] score keeper finished

--- DOM/ScoreKpr/scoreKpr.js
+++ DOM/ScoreKpr/scoreKpr.js
@@ -1,8 +1,10 @@
 var p1Btn = document.querySelector("#p1Btn");
 var p2Btn = document.getElementById("p2Btn");
 var resetBtn = document.getElementById("resetBtn");
-var p1Score = document.getElementById("p1Score");
-var p2Score = document.getElementById("p2Score");
+var p1ScoreDisplay = document.getElementById("p1Score");
+var p2ScoreDisplay = document.getElementById("p2Score");
+var numInput = document.querySelector("input");
+var winScoreDisplay = document.querySelector("p span");
 var p1ScoreRet = 0;
 var p2ScoreRet = 0;
 var gameOver = false;
@@ -12,10 +14,10 @@ p1Btn.addEventListener("click", function(){
     if(!gameOver){
         p1ScoreRet++;
         if(p1ScoreRet === winScore){
-            p1Score.classList.toggle("winner");
+            p1ScoreDisplay.classList.add("winner");
             gameOver = true;
         }
-        p1Score.textContent = p1ScoreRet;
+        p1ScoreDisplay.textContent = p1ScoreRet;
     }
 });
 
@@ -23,19 +25,29 @@ p2Btn.addEventListener("click", function(){
     if(!gameOver){
         p2ScoreRet++;
         if(p2ScoreRet === winScore){
-            p2Score.classList.toggle("winner");
+            p2ScoreDisplay.classList.add("winner");
             gameOver = true;
         }
-        p2Score.textContent = p2ScoreRet;
+        p2ScoreDisplay.textContent = p2ScoreRet;
     }
 });
 
-resetBtn.addEventListener("click", function(){
+function reset(){
     p1ScoreRet = 0;
     p2ScoreRet = 0;
-    p1Score.textContent = p1ScoreRet;
-    p2Score.textContent = p2ScoreRet;
-    p1Score.classList.remove("winner");
-    p2Score.classList.remove("winner");
+    p1ScoreDisplay.textContent = 0;
+    p2ScoreDisplay.textContent = 0;
+    p1ScoreDisplay.classList.remove("winner");
+    p2ScoreDisplay.classList.remove("winner");
     gameOver = false;
+}
+
+resetBtn.addEventListener("click", function(){
+    reset();
+});
+
+numInput.addEventListener("change", function(){
+    winScoreDisplay.textContent = this.value;
+    winScore = Number(this.value);
+    reset();
 });

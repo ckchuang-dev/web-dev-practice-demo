84b1b09568f265121e5b8f90e34703d59e8800d6
dez_chuang <dissaivent@gmail.com>
2016-12-18 23:03:36 +0800

    [F2E][JS][JQUERY]Create and delete items

--- JS/jQuery/TodoListProject/assets/js/todos.js
+++ JS/jQuery/TodoListProject/assets/js/todos.js
@@ -1,1 +1,21 @@
-alert("CONNECTED!");
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut("1000", function(){
+        $(this).remove();
+    });
+    //to avoid bubble event
+    event.stopPropagation();
+});
+
+$("input[type='text']").on("keypress", function(event){
+    //hit enter
+    if(event.which === 13){
+        //grab text from input
+        var toDoItem = $(this).val();
+        //create new li into ul
+        $("ul").append("<li><span>X</span> " + toDoItem + "</li>");
+    }
+});

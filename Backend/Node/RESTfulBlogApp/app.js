c4bce6476f251d073676e66d55ba3baaa7f8a1af
dez_chuang <Dez_Chuang@compal.com>
2017-02-06 21:00:20 +0800

    [B2E][Node][RESTful]Add NEW and CREATE route in blog

--- Backend/Node/RESTfulBlogApp/app.js
+++ Backend/Node/RESTfulBlogApp/app.js
@@ -29,12 +29,31 @@ app.get("/", function(req, res){
     res.redirect("/blogs");
 });
 
+//INDEX ROUTE
 app.get("/blogs", function(req, res){
     Blog.find({}, function(err, blogs){
         if(err){
             console.log("ERROR!");
         } else {
             res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+//NEW ROUTE
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+
+//CREATE ROUTE
+app.post("/blogs", function(req, res){
+    //create blog
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            console.log(err);
+        } else {
+            //redirect to index
+            res.redirect("/blogs");
         }
     });
 });

5b3ab3786f1635fc54a6bdce98b93a020f33842c
dez_chuang <Dez_Chuang@compal.com>
2017-02-12 17:00:47 +0800

    [B2E][Node][RESTful]Add DELETE route and express-sanitizer to prevent middleware script

--- Backend/Node/RESTfulBlogApp/app.js
+++ Backend/Node/RESTfulBlogApp/app.js
@@ -1,14 +1,16 @@
-var bodyParser      = require("body-parser"),
-    methodOverride  = require("method-override"),
-    mongoose        = require("mongoose"),
-    express         = require("express"),
-    app             = express();
+var bodyParser          = require("body-parser"),
+    methodOverride      = require("method-override"),
+    expressSanitizer    = require("express-sanitizer"),
+    mongoose            = require("mongoose"),
+    express             = require("express"),
+    app                 = express();
 
 //APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 //MONGOOSE/MODEL CONFIG
@@ -19,12 +21,6 @@ var blogSchema = new mongoose.Schema({
     created: {type: Date, default: Date.now}
 });
 var Blog = mongoose.model("Blog", blogSchema);
-
-// Blog.create({
-//     title: "Test",
-//     image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg",
-//     body: "HELLO WORLD!"
-// });
 
 //RESTFUL ROUTES
 app.get("/", function(req, res){
@@ -50,6 +46,8 @@ app.get("/blogs/new", function(req, res){
 //CREATE ROUTE
 app.post("/blogs", function(req, res){
     //create blog
+    //Add express-sanitizer to prevent middleware script in post
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             console.log(err);
@@ -86,12 +84,26 @@ app.get("/blogs/:id/edit", function(req, res){
 
 //UPDATE ROUTE
 app.put("/blogs/:id", function(req, res){
+    //Add express-sanitizer to prevent middleware script in post
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
         if(err){
             res.redirect("/blogs");
             console.log("ERROR in UPDATE ROUTE");
         } else{
             res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+            console.log("ERROR in DELETE ROUTE");
+        } else {
+            res.redirect("/blogs");
         }
     });
 });

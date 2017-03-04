0a865c13ba1eb59fc38dec1eb5b8bece4c2e0520
dez_chuang <Dez_Chuang@compal.com>
2017-03-04 13:13:59 +0800

    [F2E][React][Pokemon]Pokemon dashboard init

---
+++ Frontend/React/projects/pokedashboard/src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'whatwg-fetch';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemon: []
+    };
+    this.loadPokemon = this.loadPokemon.bind(this);
+  }
+
+  loadPokemon(url) {
+    fetch(url)
+      .then(response => {
+        return response.json();
+      }).then(json => {
+        console.log(json);
+        this.setState({
+          pokemon: json.results
+        });
+        console.log(this.state);
+      }).catch(err => {
+        console.log(err)
+      })
+  }
+
+  componentWillMount() {
+    this.loadPokemon(`${this.props.baseUrl}/pokemon/`);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;

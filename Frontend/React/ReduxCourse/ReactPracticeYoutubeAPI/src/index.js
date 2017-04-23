390ef7e4eebb938039594459b42e326c0a88be6e
dez_chuang <Dez_Chuang@compal.com>
2017-04-23 22:55:08 +0800

    [F2E][React][Redux]React and Redux practice Init, youtubeAPI and bookSelect

---
+++ Frontend/React/ReduxCourse/ReactPracticeYoutubeAPI/src/index.js
@@ -0,0 +1,51 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyCRTOdNs88WGy-s1Ng-1QyPOcOUhh9-e8s';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('nightblue3');
+
+  }
+
+  videoSearch(term) {
+    YTSearch( {key: API_KEY, term: term} , (videos) => {
+      console.log(videos);
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+      //ES6 syntax, same as below
+      //this.setState({ videos: videos});
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => this.videoSearch(term), 1000);
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
---
+++ Frontend/React/ReduxCourse/ReduxPracticeBookSelect/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Harry porter', pages: 9487},
+    {title: 'Fifty shades of Gray', pages: 333},
+    {title: 'Game of Throne: The Song of Ice and Fire', pages: 29},
+    {title: 'The Hunger Game', pages: 101}
+  ]
+}
